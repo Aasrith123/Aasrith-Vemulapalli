dab2e790cc19546336e8e89316176f716603e736
Aasrith123 <146471566+Aasrith123@users.noreply.github.com>
2024-01-20 17:46:56 -0500

    Add files via upload

--- #Final Project.py
+++ #Final Project.py
@@ -21,6 +21,7 @@ def main():
     y = 400
    
     velocity = 5
+    mob_velocity = 0.5
     run = True
     
     player_image = pygame.image.load(os.path.join('Pygame', 'player_image.png'))
@@ -34,15 +35,20 @@ def main():
     bg = pygame.transform.scale(bg, (width, height))
            
     mob_image = pygame.image.load(os.path.join('Pygame','mob_wave_1.png'))
-    mob_image = pygame.transform.scale(mob_image,(40,40))
+    mob_image = pygame.transform.scale(mob_image,(70,60))
     
     mob_count = random.randint(6, 12)
 
-    mobs = []
+    mobs = [] 
     for i in range(0,mob_count+1):
-        x_mob = random.randint(0,900)
-        y_mob = random.randint(0,500)
+        x_mob = random.randint(0,width - 70)
+        y_mob = random.randint(0,height - 60)
+        mobs.append((x_mob, y_mob))
+    
+    
     while run: 
+
+
         clock.tick(FPS)
         player_hitbox = pygame.Rect(x, y, width_player, height_player)
         
@@ -51,13 +57,27 @@ def main():
                 run = False
                 pygame.quit()
                 quit()
+
         WIN.fill((0, 0, 0))  
 
         WIN.blit(bg,(0,0))
 
         WIN.blit(player_image, (x, y))  
         
-        WIN.blit(mob_image,(x_mob,y_mob))
+        for i, mob_pos in enumerate(mobs): 
+            mob_x, mob_y = mob_pos
+            mob_rect = pygame.Rect(mob_x,mob_y,70,60)
+
+            dir = pygame.math.Vector2(x - mob_x, y - mob_y).normalize()
+
+            mob_x += dir.x * mob_velocity
+            mob_y += dir.y * mob_velocity
+
+            mobs[i] = (mob_x, mob_y)
+            
+            
+            WIN.blit(mob_image, mob_pos)
+
 
         
         # PLAYER MOVEMENT
@@ -84,68 +104,3 @@ def main():
 if __name__ == "__main__":
     
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
