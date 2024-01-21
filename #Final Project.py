f8053c2e0f82d4d6128ca4ca3d44e509f063a029
Aasrith123 <146471566+Aasrith123@users.noreply.github.com>
2024-01-21 01:12:28 -0500

    Add files via upload

--- #Final Project.py
+++ #Final Project.py
@@ -11,12 +11,20 @@ clock = pygame.time.Clock()
 FPS = 60
 width, height = 1280,800
 direction = True
+player_health = 100
+
 #SCREEN AND TITLE
 WIN = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Monster Hunter!")
 
 
 def main(): 
+    
+    
+    
+    global player_health
+    damage_cooldown = 0
+
     x = 620
     y = 400
    
@@ -24,10 +32,13 @@ def main():
     mob_velocity = 0.5
     run = True
     
-    player_image = pygame.image.load(os.path.join('Pygame', 'player_image.png'))
+    width_mob = 70
+    height_mob = 60
+
     width_player = 50  
     height_player = 50
 
+    player_image = pygame.image.load(os.path.join('Pygame', 'player_image.png'))
     player_image = pygame.transform.scale(player_image, (width_player, height_player))
     player_copy = player_image.copy()
     
@@ -38,37 +49,47 @@ def main():
     mob_image = pygame.transform.scale(mob_image,(70,60))
     
     mob_count = random.randint(6, 12)
-
+    mob_health = 100 
     mobs = [] 
+    
+    
     for i in range(0,mob_count+1):
-        x_mob = random.randint(0,width - 70)
-        y_mob = random.randint(0,height - 60)
+        x_mob = random.randint(0,width - width_mob)
+        y_mob = random.randint(0,height - height_mob)
         mobs.append((x_mob, y_mob))
     
     
     while run: 
-
-
-        clock.tick(FPS)
-        player_hitbox = pygame.Rect(x, y, width_player, height_player)
-        
+        #EXIT CODE
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
                 run = False
                 pygame.quit()
                 quit()
 
-        WIN.fill((0, 0, 0))  
-
-        WIN.blit(bg,(0,0))
-
-        WIN.blit(player_image, (x, y))  
+        global player_health
+        #Refresh rate
+        clock.tick(FPS)
+        #Player hitboxing
+        player_hitbox = pygame.Rect(x, y, width_player, height_player)
         
+        player_health_words = font_player_health.render("Player Health", True,(255,255,255))
+        #Bliting and things like that to make the background and player image show
+        WIN.fill((0, 0, 0))  
+        WIN.blit(bg,(0,0))
+        WIN.blit(player_image, (x, y))
+        WIN.blit(player_health_words,(20,40))
+        #Mob Spawning
         for i, mob_pos in enumerate(mobs): 
             mob_x, mob_y = mob_pos
             mob_rect = pygame.Rect(mob_x,mob_y,70,60)
 
             dir = pygame.math.Vector2(x - mob_x, y - mob_y).normalize()
+            angle = dir.angle_to(pygame.math.Vector2(1,0))
+
+            rotated_mob_image = pygame.transform.rotate(mob_image,angle)
+            rotated_mob_rect = rotated_mob_image.get_rect(center=(mob_x,mob_y))
+
 
             mob_x += dir.x * mob_velocity
             mob_y += dir.y * mob_velocity
@@ -76,10 +97,19 @@ def main():
             mobs[i] = (mob_x, mob_y)
             
             
-            WIN.blit(mob_image, mob_pos)
-
-
-        
+            WIN.blit(rotated_mob_image, rotated_mob_rect.topleft)
+        for i in 
+        #If playerhitbox colides with the mobs hitbox damage is taken
+            if player_hitbox.colliderect(mob_rect) and damage_cooldown==0: 
+                player_health-= 10
+                damage_cooldown =60 # 60 change after you test the game over screen
+        #DAMAGE COOLDOWN 60 - 1 till its 0 to achieve a cooldown of 0 
+        if damage_cooldown>0:
+            damage_cooldown -=1
+        #This creates a player health bar 
+        pygame.draw.rect(WIN,(255,255,255), (18,18,204,24),2) # WHITE OUTLINE 
+        pygame.draw.rect(WIN,(255,0,0),(20,20,200,20)) # RED BG OF HEALTH BAR 
+        pygame.draw.rect(WIN, (0,255,0), (20,20, player_health * 2, 20)) # GREEN HEALTH BAR 
         # PLAYER MOVEMENT
         key_pressed_is = pygame.key.get_pressed()
         if key_pressed_is[K_LEFT] and x - velocity >= 0:
@@ -100,7 +130,20 @@ def main():
 
         pygame.display.flip()  
 
+        if player_health <=0:
+            game_over = font.render("Game Over!", True,(200,0,0))
+            WIN.blit(game_over,(450, 250))
+            pygame.display.flip()
+            pygame.time.delay(5000)
+            if pygame.time.get_ticks() - start_time > 5000:  # 5000 change after testing game over screen
+                run = False
+           
+            
+
     
 if __name__ == "__main__":
-    
+    start_time = pygame.time.get_ticks()
+    font = pygame.font.SysFont("pristina",100,True)
+    font_player_health = pygame.font.SysFont("comicsans", 30, True)
     main()
+    
